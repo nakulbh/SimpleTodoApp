c80f1203f493426142d99990eefe65587e6f8628
nakulbh <nakulbhardwaj37@gmail.com>
2024-02-12 04:40:18 +0530

    making backend in golang

--- goBackend/dbSchema.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type todo struct {
-	title       string `bson:"title,omitempty"`
-	description string `bson:"description,omitempty"`
-	completed   bool   `bson:"completed,omitempty"`
-}
-
-
--- goBackend/main.go
+++ goBackend/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
+
+
+type todo struct {
+	Title       string `bson:"title,omitempty"`
+	Description string `bson:"description,omitempty"`
+	Completed   bool   `bson:"completed,omitempty"`
+}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
 		log.Fatal("Error loading .env file")
-
 	}
 
 	uri := os.Getenv("MONGODB_URI")
@@ -20,29 +31,84 @@ func main() {
 		log.Fatal("MONGODB_URI not set")
 	}
 
-
 	client, ctx, cancel, err := connect(uri)
 	if err != nil {
 		panic(err)
 	}
+	defer close(client, ctx, cancel)
+
+	if err := ping(client, ctx); err != nil {
+		panic(err)
+	}
 
 	database := client.Database("todos")
-
 	todosCollection := database.Collection("todos")
 
 	todo := todo{
-		title:       "Dsa learning",
-		description: "learning Dsa for exams",
-		completed:   true,
+		Title:       "MERN learning",
+		Description: "learning MERN for jobs",
+		Completed:   true,
 	}
 
 	insertResult, err := todosCollection.InsertOne(ctx, todo)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(insertResult.InsertedID)
+	fmt.Println("Inserted ID:", insertResult.InsertedID)
+}
 
-	defer close(client, ctx, cancel)
-	ping(client, ctx)
 
+func close(client *mongo.Client, ctx context.Context,
+	cancel context.CancelFunc) {
+
+	// CancelFunc to cancel to context
+	// CancelFunc tells an operation to abandon its work.
+	defer cancel()
+
+	// client provides a method to close
+	// a mongoDB connection.
+	defer func() {
+
+		// client.Disconnect method also has deadline.
+		// returns error if any,
+		if err := client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+}
+
+// This is a user defined method that returns mongo.Client,
+// context.Context, context.CancelFunc and error.
+// mongo.Client will be used for further database operation.
+// context.Context will be used set deadlines for process.
+// context.CancelFunc will be used to cancel conteMarka as xt and
+// resource associated with it.
+
+func connect(uri string) (*mongo.Client, context.Context,
+	context.CancelFunc, error) {
+
+	// ctx will be used to set deadline for process, here
+	// deadline will of 30 seconds.
+	ctx, cancel := context.WithTimeout(context.Background(),
+		30*time.Second)
+
+	// mongo.Connect return mongo.Client method
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(uri))
+	return client, ctx, cancel, err
+}
+
+// This is a user defined method that accepts
+// mongo.Client and context.Context
+// This method used to ping the mongoDB, return error if any.
+func ping(client *mongo.Client, ctx context.Context) error {
+
+	// mongo.Client has Ping to ping mongoDB, deadline of
+	// the Ping method will be determined by cxt
+	// Ping method return error if any occurred, then
+	// the error can be handled.
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		return err
+	}
+	fmt.Println("connected successfully")
+	return nil
 }
